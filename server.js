1f28a6681be0e704ee1dffbac45a0f775bf57bb3
Boris B. Herrera <boris.herrerac@gmail.com>
2019-06-21 23:06:30 +0000

    test mongo connection

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 'use strict';
 // Module Dependencies
 // -------------------
+require('dotenv').config();
 const express     = require('express');
 const path        = require('path');
 const request     = require('request');
 const routes      = require('./routes');
+const mongoose    = require('mongoose');
 
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
+
+mongoose.set('useNewUrlParser', true);
+if(process.env.MONGODB_URI) {
+    mongoose.connect(process.env.MONGODB_URI);
+}
+else {
+    mongoose.connect(process.env.PRE_MONGODB);
+}
 
 app.get('/', routes.index);
 
